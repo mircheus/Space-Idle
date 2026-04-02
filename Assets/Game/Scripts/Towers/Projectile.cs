4dc51a282af498f964ab287fd31b4f578f26f0ab
Miroslav Cherniavskii <mircheus@gmail.com>
2026-04-02 11:04:08 +0300

    WIP projectiles for towers, without Injection yet

---
+++ Assets/Game/Scripts/Towers/Projectile.cs
@@ -0,0 +1,44 @@
+﻿using Game.Scripts;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 12f;
+    [SerializeField] private int damage = 20;
+    [SerializeField] private float maxLifetime = 5f;     // авто-уничтожение
+
+    private Enemy _target;
+    private Rigidbody _rb;
+
+    private void Awake() => _rb = GetComponent<Rigidbody>();
+
+    /// <summary>Вызывается башней сразу после Instantiate.</summary>
+    public void Init(Enemy target)
+    {
+        _target = target;
+        Destroy(gameObject, maxLifetime);                // фолбэк на случай промаха
+    }
+
+    private void FixedUpdate()
+    {
+        if (_target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // Наводимся на текущую позицию цели (homing)
+        var dir = (_target.transform.position - transform.position).normalized;
+        _rb.linearVelocity = dir * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.TryGetComponent<Enemy>(out var enemy)) return;
+        if (enemy != _target) return;                   // игнорируем других врагов
+
+        enemy.TakeDamage(damage);
+        Destroy(gameObject);
+    }
+}
--- Assets/Game/Scripts/Towers/Tower.cs
+++ Assets/Game/Scripts/Towers/Tower.cs
@@ -1,16 +1,104 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
+using Object = UnityEngine.Object;
 
 namespace Game.Scripts
 {
     public class Tower : MonoBehaviour
     {
-        private TowerType _towerType;
+        [Header("References:")]
+        [SerializeField] private Transform firePoint;
         
+        [Header("Inject this instead")]
+        [SerializeField] private Projectile projectilePrefab;
+        [SerializeField] private float fireRate;
+        
+        private TowerType _towerType; 
+        private Collider2D _collider2D;
+        private Enemy  _currentTarget;
+        private Coroutine _fireCoroutine;
+        
+        private List<Enemy> _enemiesInRange = new List<Enemy>(); 
+
         [Inject]
         public void Construct(TowerType towerType)
         {
             _towerType = towerType;
+        }
+
+        private void Awake()
+        {
+            _collider2D = GetComponent<Collider2D>();
+            _collider2D.isTrigger = true;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.TryGetComponent<Enemy>(out var enemy))
+            {
+                _enemiesInRange.Add(enemy);
+            }
+            
+            if (_currentTarget != null) return;          // уже есть цель
+            
+            SetTarget(enemy);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.TryGetComponent<Enemy>(out var enemy))
+            {
+                if(_currentTarget == enemy)
+                    ClearTarget();
+
+                if (_enemiesInRange.Contains(enemy))
+                    _enemiesInRange.Remove(enemy);
+            }
+        }
+
+        private void SetTarget(Enemy enemy)
+        {
+            _currentTarget = enemy;
+            _fireCoroutine = StartCoroutine(FireLoop());
+        }
+
+        private void ClearTarget()
+        {
+            _currentTarget = null;
+
+            if (_fireCoroutine != null)
+            {
+                StopCoroutine(_fireCoroutine);
+                _fireCoroutine = null;
+            }
+        }
+
+        private void Shoot()
+        {
+            var go = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+
+            if (go.TryGetComponent<Projectile>(out var projectile))
+                projectile.Init(_currentTarget);
+        }
+        
+        private IEnumerator FireLoop()
+        {
+            if (_currentTarget.IsAlive == false)
+            {
+                ClearTarget();
+                yield break;
+            }
+            
+            var interval = new WaitForSeconds(1f / fireRate);
+
+            while (_currentTarget != null)
+            {
+                Shoot();
+                yield return interval;
+            }
         }
         
         public class Factory : PlaceholderFactory<TowerType, Tower> { }
